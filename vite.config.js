88b3c60d45c16f9e7deac24d0f00c5af89b01dfe
sebadev-img <sebastiandev.img@gmail.com>
2023-03-02 21:46:56 -0300

    config added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/empleo-educacion/",
   plugins: [react()],
+  base: "/empleo-educacion/",
 });
